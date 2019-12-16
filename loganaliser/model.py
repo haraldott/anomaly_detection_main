Take out softmax, fix output of loss distribution to txt file

loganaliser/model.py
@@ -36,8 +36,8 @@ class LSTM(nn.Module):
         output = self.dropout(output)
         decoded = self.decoder(output)
         # TODO: log_softmax makes everything 0 can we leave it like this?
-        decoded_scores = F.log_softmax(decoded, dim=1)
-        return decoded_scores, hidden
+        # decoded_scores = F.log_softmax(decoded, dim=1)
+        return decoded, hidden
 
     def init_hidden(self, bsz, device):
         weight = next(self.parameters())