1c4da24d12d1bbb60c925756c2f0af472fab05ab
Harald Ott <>
2020-03-07 16:01:32 +0100

    add some parsed scripts, add commands script

--- commands.sh
+++ commands.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_deleted_lines" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/anomalies/18k_spr_deleted.pickle" -anomaly_description="deleted_lines"
 
-python meta_script_bert.py -hiddenunits=768 -epochs=1
-python meta_script.py -embeddingsize=100 -hiddenunits=100 -epochs=80
-python meta_script.py -embeddingsize=100 -hiddenunits=100 -epochs=100
-python meta_script.py -embeddingsize=100 -hiddenunits=100 -epochs=150
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_duplicated_lines" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/anomalies/18k_spr_duplicated.pickle" -anomaly_description="duplicated_lines" -anomaly_only
 
-python meta_script.py -embeddingsize=200 -hiddenunits=200 -epochs=80
-python meta_script.py -embeddingsize=200 -hiddenunits=200 -epochs=100
-python meta_script.py -embeddingsize=200 -hiddenunits=200 -epochs=150
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_injected_2_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="injected_2_words" -anomaly_only
 
-python meta_script.py -embeddingsize=250 -hiddenunits=250 -epochs=80
-python meta_script.py -embeddingsize=250 -hiddenunits=250 -epochs=100
-python meta_script.py -embeddingsize=250 -hiddenunits=250 -epochs=150
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_injected_3_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="injected_3_words" -anomaly_only
 
-python meta_script_bert.py -hiddenunits=768 -epochs=50
-python meta_script_bert.py -hiddenunits=768 -epochs=75
-python meta_script_bert.py -hiddenunits=768 -epochs=100
-python meta_script_bert.py -hiddenunits=768 -epochs=150
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_injected_4_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="injected_4_words" -anomaly_only
+
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_injected_5_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="injected_5_words" -anomaly_only
+
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_injected_6_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="injected_6_words" -anomaly_only
+
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_removed_1_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="removed_1_words" -anomaly_only
+
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_removed_2_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="removed_2_words" -anomaly_only
+
+python meta_script_bert.py -corpus_anomaly_inputfile="data/openstack/utah/parsed/anomalies_injected/18k_spr_removed_3_words" -instance_information_file_anomalies="data/openstack/utah/raw/sorted_per_request_pickle/18k_spr.pickle" -anomaly_description="removed_3_words" -anomaly_only
