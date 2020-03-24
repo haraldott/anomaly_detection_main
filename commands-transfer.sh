e5ba39775f7380c6c3d52ef93bbf97049fc6d82a
Harald Ott <>
2020-03-24 09:18:21 +0100

    add cuda visible devices = 1

--- commands-transfer.sh
+++ commands-transfer.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 # Transfer learning
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=1
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=2 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=3 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=4 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=5 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=6 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=7 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=8 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=9 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=1 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=2 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=3 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=1 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=2 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=3 -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="duplicate_lines" -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="delete_lines" -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="random_lines" -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="shuffle" -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="no_anomaly" -anomaly_only
-python transfer_learning.py -finetune -anomaly_type="reverse_order" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=1
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=2 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=3 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=4 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=5 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=6 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=7 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=8 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="insert_words" -anomaly_amount=9 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=1 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=2 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="remove_words" -anomaly_amount=3 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=1 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=2 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="replace_words" -anomaly_amount=3 -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="duplicate_lines" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="delete_lines" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="random_lines" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="shuffle" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="no_anomaly" -anomaly_only
+CUDA_VISIBLE_DEVICES=1 python transfer_learning.py -finetune -anomaly_type="reverse_order" -anomaly_only
