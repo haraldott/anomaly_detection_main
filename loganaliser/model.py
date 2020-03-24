5a52da41b70e26b8e4a17f81982c035f0869aca1
Harald Ott <>
2020-03-24 09:06:56 +0100

    increase dropout

--- loganaliser/model.py
+++ loganaliser/model.py
@@ -10,12 +10,11 @@ class LSTM(nn.Module):
         self.train_mode = train_mode
 
         # Layers
-        self.dropout = nn.Dropout(dropout)
 
         # self.attn = nn.Linear(self.n_hidden_units * 2, n_input)
         # self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         self.lstm = nn.LSTM(input_size=n_input, hidden_size=n_hidden_units, num_layers=n_layers,
-                            dropout=dropout, batch_first=True)
+                            dropout=0.5, batch_first=True)
         self.decoder = nn.Linear(n_hidden_units, n_input)
 
         if tie_weights:
@@ -34,7 +33,7 @@ class LSTM(nn.Module):
 
     def forward(self, input, hidden):
         if self.train_mode:
-            input = self.dropout(input)
+            input = nn.Dropout(0.3)
 
         # attn_weights = F.softmax(self.attn(torch.cat((input[0], hidden[0]), 1)), dim=1)
 
