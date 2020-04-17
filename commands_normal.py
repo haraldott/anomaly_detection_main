09a264e7f2996960d145c8a58ada9dd6cb865152
Harald Ott <>
2020-04-17 11:38:15 +0200

    Refactor

--- commands_normal.py
+++ commands_normal.py
@@ -1,28 +1,21 @@
 from normal_learning import experiment
 
-# hyperparameters
-seq_len= [7, 8, 9, 10]
-n_layers = [1, 2, 3]
-n_hidden_units= [128, 256, 512, 768]
-batch_size = [64, 128]
-clip = [0.9, 0.95, 1.0, 1.05, 1.1]
-epochs = [80, 90, 100, 110, 120, 130]
-
-score_file = open("score_file.txt", 'w')
-
-for len in seq_len:
-    for layer in n_layers:
-        for hidden in n_hidden_units:
-            for batch in batch_size:
-                for cl in clip:
-                    for epoch in epochs:
-                        print("seq_len: {}, n_layers: {}, n_hidden_units: {}, batch_size: {}, clip: {}, epochs: {}\n"
-                              .format(len, layer, hidden, batch, cl, epoch))
-                        f1_score, precision = experiment(seq_len=len, n_layers=layer, n_hidden_units=hidden, batch_size=batch,
-                                              clip=cl, epochs=epoch)
-                        score_file.write("F1-Score: {}, Precision: {}, ----- "
-                                         "seq_len: {}, n_layers: {}, n_hidden_units: {}, batch_size: {}, clip: {}, epochs: {}\n"
-                                         .format(f1_score, precision, len, layer, hidden, batch, cl, epoch))
-score_file.close()
-
-# continue here: seq_len: 7, n_layers: 2, n_hidden_units: 256, batch_size: 64, clip: 0.9, epochs: 80
+experiment(anomaly_type="random_lines")
+experiment(anomaly_type="insert_words", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=2, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=3, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=4, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=5, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=6, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=7, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=8, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="insert_words", anomaly_amount=9, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="remove_words", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="remove_words", anomaly_amount=2, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="replace_words", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="replace_words", anomaly_amount=2, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="duplicate_lines", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="delete_lines", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="shuffle", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="no_anomaly", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
+experiment(anomaly_type="reverse_order", anomaly_amount=1, anomaly_only=True, label_encoder="encoder_normal.pickle")
---
+++ grid_search.py
@@ -0,0 +1,28 @@
+from normal_learning import experiment
+
+# hyperparameters
+seq_len= [7, 8, 9, 10]
+n_layers = [1, 2, 3]
+n_hidden_units= [128, 256, 512, 768]
+batch_size = [64, 128]
+clip = [0.9, 0.95, 1.0, 1.05, 1.1]
+epochs = [80, 90, 100, 110, 120, 130]
+
+score_file = open("grid_search_score_file.txt", 'w')
+
+for len in seq_len:
+    for layer in n_layers:
+        for hidden in n_hidden_units:
+            for batch in batch_size:
+                for cl in clip:
+                    for epoch in epochs:
+                        print("seq_len: {}, n_layers: {}, n_hidden_units: {}, batch_size: {}, clip: {}, epochs: {}\n"
+                              .format(len, layer, hidden, batch, cl, epoch))
+                        f1_score, precision = experiment(seq_len=len, n_layers=layer, n_hidden_units=hidden, batch_size=batch,
+                                              clip=cl, epochs=epoch)
+                        score_file.write("F1-Score: {}, Precision: {}, ----- "
+                                         "seq_len: {}, n_layers: {}, n_hidden_units: {}, batch_size: {}, clip: {}, epochs: {}\n"
+                                         .format(f1_score, precision, len, layer, hidden, batch, cl, epoch))
+score_file.close()
+
+# continue here: seq_len: 7, n_layers: 2, n_hidden_units: 256, batch_size: 64, clip: 0.9, epochs: 80
