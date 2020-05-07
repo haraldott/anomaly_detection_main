369edbfc8818351aa78c39bd4af6a31dceb55b40
Harald Ott <>
2020-05-07 16:23:09 +0200

    Hotfix - correct multiclas script

--- commands_classification.py
+++ commands_classification.py
@@ -1,26 +1,26 @@
 from normal_learning import experiment
 
 experiment(epochs=100,
-           mode="classification",
+           mode="multiclass",
            anomaly_type='random_lines',
            anomaly_amount=1,
            clip=1.0,
            anomaly_only=False,
-           experiment="classification")
-experiment(anomaly_type='insert_words', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='insert_words', anomaly_amount=2, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='insert_words', anomaly_amount=3, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='insert_words', anomaly_amount=4, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='remove_words', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='remove_words', anomaly_amount=2, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='replace_words', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='replace_words', anomaly_amount=2, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='duplicate_lines', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='duplicate_lines', anomaly_amount=2, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='duplicate_lines', anomaly_amount=3, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='delete_lines', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='delete_lines', anomaly_amount=2, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='shuffle', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='no_anomaly', anomaly_amount=1, experiment="classification", anomaly_only=True)
-experiment(anomaly_type='reverse_order', anomaly_amount=1, experiment="classification", anomaly_only=True)
+           experiment="multiclass")
+experiment(anomaly_type='insert_words', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='insert_words', anomaly_amount=2, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='insert_words', anomaly_amount=3, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='insert_words', anomaly_amount=4, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='remove_words', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='remove_words', anomaly_amount=2, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='replace_words', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='replace_words', anomaly_amount=2, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='duplicate_lines', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='duplicate_lines', anomaly_amount=2, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='duplicate_lines', anomaly_amount=3, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='delete_lines', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='delete_lines', anomaly_amount=2, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='shuffle', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='no_anomaly', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
+experiment(anomaly_type='reverse_order', anomaly_amount=1, experiment="multiclass", anomaly_only=True)
 
