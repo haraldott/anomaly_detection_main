5b37bc16368f96653fa44669c1baf5a438b6df40
Harald Ott <>
2020-05-07 15:41:22 +0200

    change percentile

--- helper_scripts/calculate_values_on_textfiles.py
+++ helper_scripts/calculate_values_on_textfiles.py
@@ -30,12 +30,12 @@ def calc_percentile_outliers(normal_loss_values_path="/Users/haraldott/Downloads
 
 
 
-def calc_f1_based_on_percentile(normal_loss_values_path="/Users/haraldott/Downloads/results/regression/bert_epochs_130_seq_len_7_description__random_lines_1/normal_loss_values",
-                                anomaly_loss_values_path="/Users/haraldott/Downloads/results/regression/bert_epochs_130_seq_len_7_description__random_lines_1/anomaly_loss_values",
-                                ground_truth_path="/Users/haraldott/Downloads/results/regression/bert_epochs_130_seq_len_7_description__random_lines_1/true_anomaly_labels.txt"):
+def calc_f1_based_on_percentile(normal_loss_values_path="/Users/haraldott/Downloads/regression - strange behaviour, spikes during training/normal_loss_values",
+                                anomaly_loss_values_path="/Users/haraldott/Downloads/regression - strange behaviour, spikes during training/anomaly_loss_values",
+                                ground_truth_path="/Users/haraldott/Downloads/regression - strange behaviour, spikes during training/true_anomaly_labels.txt"):
     with open(ground_truth_path) as f:
         ground_truth = [int(y) for y in f.readlines()]
-    pred_indeces = calc_percentile_outliers(normal_loss_values_path, anomaly_loss_values_path, 100)
+    pred_indeces = calc_percentile_outliers(normal_loss_values_path, anomaly_loss_values_path, 99.2)
 
     with open(anomaly_loss_values_path) as f:
         anomaly_loss_values = f.readlines()
