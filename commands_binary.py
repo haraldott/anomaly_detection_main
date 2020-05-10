467040b8510abc44a7b75e66e0ad5f084b8b1081
Harald Ott <>
2020-05-10 13:30:01 +0200

    Minor - fix commands binary

--- commands_binary.py
+++ commands_binary.py
@@ -1,8 +1,8 @@
 from normal_learning import experiment
 
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:0.9", clip=0.9)
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:0.95", clip=0.95)
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.0", clip=1.0)
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.05", clip=1.05)
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.1", clip=1.1)
-experiment(anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.15", clip=1.15)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:0.9", clip=0.9)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:0.95", clip=0.95)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.0", clip=1.0)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.05", clip=1.05)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.1", clip=1.1)
+experiment(mode="binary", anomaly_type='random_lines', anomaly_amount=1, experiment="binary_clip:1.15", clip=1.15)
