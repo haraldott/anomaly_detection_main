8c51f7e253d28f7f4c849465da4bd28e2dc59bc3
Harald Ott <>
2020-05-12 13:21:25 +0200

    Minor, increase epochs

--- normal_learning.py
+++ normal_learning.py
@@ -17,7 +17,7 @@ from wordembeddings.visualisation import write_to_tsv_files_bert_sentences
 from shared_functions import get_embeddings
 
 
-def experiment(epochs=30,
+def experiment(epochs=80,
                mode="multiclass",
                anomaly_type='random_lines',
                anomaly_amount=1,
