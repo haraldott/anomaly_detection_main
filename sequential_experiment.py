eb0224c45a020c63f84fc16d7ae6c4c8e13bf4e6
Harald Ott <>
2020-07-07 10:22:42 +0200

    fix experiemnts

--- sequential_experiment.py
+++ sequential_experiment.py
@@ -4,12 +4,17 @@ from normal_learning import experiment
 results_folder = 'results_sequential/'
 makedirs(results_folder, exist_ok=True)
 
+
+experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
+                                   embeddings_model='bert')
+
 alteration_ratio = 0.05
 with open(results_folder + 'bert_multiclass_duplicate_lines_results_anomaly_ratio_0.05.txt', 'w') as results:
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -19,7 +24,7 @@ with open(results_folder + 'bert_multiclass_delete_lines_results_anomaly_ratio_0
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='delete_lines', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -29,7 +34,7 @@ with open(results_folder + 'bert_multiclass_shuffle_results_anomaly_ratio_0.05.t
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='shuffle', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -40,7 +45,7 @@ with open(results_folder + 'bert_regression_duplicate_lines_results_anomaly_rati
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -50,7 +55,7 @@ with open(results_folder + 'bert_regression_delete_lines_results_anomaly_ratio_0
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='delete_lines', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -60,7 +65,7 @@ with open(results_folder + 'bert_regression_shuffle_results_anomaly_ratio_0.05.t
     while alteration_ratio <= 0.2:
         f1, precision = experiment(anomaly_type='shuffle', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
--- words_alterations_experiment.py
+++ words_alterations_experiment.py
@@ -5,12 +5,15 @@ from normal_learning import experiment
 results_folder = 'results_qualitative/'
 makedirs(results_folder, exist_ok=True)
 
+experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
+                                   embeddings_model='bert')
 alteration_ratio = 0.05
 with open(results_folder + 'bert_multiclass_insert_words_results_anomaly_ratio_0.04.txt', 'w') as results:
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -23,7 +26,7 @@ with open(results_folder + 'bert_multiclass_remove_words_results_anomaly_ratio_0
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='remove_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -35,7 +38,7 @@ with open(results_folder + 'bert_multiclass_replace_words_results_anomaly_ratio_
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='replace_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -47,7 +50,7 @@ with open(results_folder + 'bert_regression_insert_words_results_anomaly_ratio_0
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='insert_words', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -60,7 +63,7 @@ with open(results_folder + 'bert_regression_remove_words_results_anomaly_ratio_0
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='remove_words', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
@@ -72,7 +75,7 @@ with open(results_folder + 'bert_regression_replace_words_results_anomaly_ratio_
     while alteration_ratio <= 0.3:
         f1, precision = experiment(anomaly_type='replace_words', anomaly_amount=1, mode="regression",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='bert')
+                                   embeddings_model='bert', experiment="alteration_ratio:{}".format(alteration_ratio))
         results.write("{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision))
         results.flush()
         alteration_ratio += 0.05
