e8378071567da6f4535dbd79db3c994689831f05
Harald Ott <>
2020-07-09 13:25:01 +0200

    update experiments

--- sequential_experiment.py
+++ sequential_experiment.py
@@ -5,16 +5,16 @@ results_folder = 'results_sequential/'
 makedirs(results_folder, exist_ok=True)
 
 
-experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
-                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
-                                   embeddings_model='gpt2', epochs=100)
+# experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
+#                                    prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
+#                                    embeddings_model='gpt2', epochs=100)
 
 alteration_ratio = 0.05
 with open(results_folder + 'gpt2_multiclass_duplicate_lines_results_anomaly_ratio_0.05.txt', 'w') as results:
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='gpt2', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
@@ -24,7 +24,7 @@ with open(results_folder + 'gpt2_multiclass_delete_lines_results_anomaly_ratio_0
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='delete_lines', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='gpt2', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
@@ -34,10 +34,55 @@ with open(results_folder + 'gpt2_multiclass_shuffle_results_anomaly_ratio_0.05.t
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='shuffle', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='gpt2', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+
+
+#######################
+#######################
+# BERT
+#######################
+#######################
+
+experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
+                                   embeddings_model='bert', epochs=100)
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_duplicate_lines_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_delete_lines_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='delete_lines', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_shuffle_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='shuffle', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+
+
 
 
 #######
--- words_alterations_experiment.py
+++ words_alterations_experiment.py
@@ -5,15 +5,15 @@ from normal_learning import experiment
 results_folder = 'results_qualitative/'
 makedirs(results_folder, exist_ok=True)
 
-# experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
-#                                    prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
-#                                    embeddings_model='bert')
+experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=0.05,
+                                   embeddings_model='bert', epochs=100)
 alteration_ratio = 0.05
-with open(results_folder + 'gpt2_multiclass_insert_words_results_anomaly_ratio_0.05.txt', 'a') as results:
+with open(results_folder + 'bert_multiclass_insert_words_results_anomaly_ratio_0.05.txt', 'w') as results:
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='bert', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
@@ -22,11 +22,11 @@ with open(results_folder + 'gpt2_multiclass_insert_words_results_anomaly_ratio_0
 ########################
 
 alteration_ratio = 0.05
-with open(results_folder + 'gpt2_multiclass_remove_words_results_anomaly_ratio_0.05.txt', 'a') as results:
+with open(results_folder + 'bert_multiclass_remove_words_results_anomaly_ratio_0.05.txt', 'w') as results:
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='remove_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='bert', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
@@ -34,11 +34,52 @@ with open(results_folder + 'gpt2_multiclass_remove_words_results_anomaly_ratio_0
 ########################
 
 alteration_ratio = 0.05
-with open(results_folder + 'gpt2_multiclass_replace_words_results_anomaly_ratio_0.05.txt', 'a') as results:
+with open(results_folder + 'bert_multiclass_replace_words_results_anomaly_ratio_0.05.txt', 'w') as results:
     while alteration_ratio <= 0.15:
         f1, precision, recall = experiment(anomaly_type='replace_words', anomaly_amount=1, mode="multiclass",
                                    prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2')
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+#######################
+#######################
+# GPT 2
+#######################
+#######################
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_insert_words_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='insert_words', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+
+########################
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_remove_words_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='remove_words', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.05, 2)
+
+########################
+
+alteration_ratio = 0.05
+with open(results_folder + 'bert_multiclass_replace_words_results_anomaly_ratio_0.05.txt', 'w') as results:
+    while alteration_ratio <= 0.15:
+        f1, precision, recall = experiment(anomaly_type='replace_words', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=True, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.05, 2)
