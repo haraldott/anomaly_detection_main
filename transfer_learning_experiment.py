157a844567e1da1297a4366af577709f52c1613a
Harald Ott <>
2020-07-14 11:17:56 +0200

    transfer learning experiments

--- transfer_learning_experiment.py
+++ transfer_learning_experiment.py
@@ -5,20 +5,20 @@ results_folder = 'results_transfer/'
 makedirs(results_folder, exist_ok=True)
 
 # Bert multiclass
-# alteration_ratio = 0.02
-# with open(results_folder + 'bert_classification_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-#     while alteration_ratio <= 0.04:
-#         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
-#                                    prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-#                                    embeddings_model='bert', epochs=100)
-#         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
-#         results.flush()
-#         alteration_ratio = round(alteration_ratio + 0.01, 2)
+alteration_ratio = 0.05
+with open(results_folder + 'bert_classification_transfer_results_anomaly_ratio_0.05.txt', 'a') as results:
+    while alteration_ratio <= 0.05:
+        f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
+                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+                                   embeddings_model='bert', epochs=100)
+        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+        results.flush()
+        alteration_ratio = round(alteration_ratio + 0.01, 2)
 
 # Bert regression
-alteration_ratio = 0.02
-with open(results_folder + 'bert_regression_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-    while alteration_ratio <= 0.04:
+alteration_ratio = 0.05
+with open(results_folder + 'bert_regression_transfer_results_anomaly_ratio_0.05.txt', 'a') as results:
+    while alteration_ratio <= 0.05:
         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="regression",
                                            prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
                                            embeddings_model='bert', epochs=100)
@@ -26,34 +26,34 @@ with open(results_folder + 'bert_regression_transfer_results_anomaly_ratio_0.05.
         results.flush()
         alteration_ratio = round(alteration_ratio + 0.01, 2)
 
-# GPT multiclass
-alteration_ratio = 0.02
-with open(results_folder + 'gpt2_classification_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-    while alteration_ratio <= 0.04:
-        f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
-                                   prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                   embeddings_model='gpt2', epochs=100)
-        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
-        results.flush()
-        alteration_ratio = round(alteration_ratio + 0.01, 2)
-
-# GPT regression
-alteration_ratio = 0.02
-with open(results_folder + 'gpt2_regression_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-    while alteration_ratio <= 0.04:
-        f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="regression",
-                                           prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
-                                           embeddings_model='gpt2', epochs=100)
-        results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
-        results.flush()
-        alteration_ratio = round(alteration_ratio + 0.01, 2)
+# # GPT multiclass
+# alteration_ratio = 0.02
+# with open(results_folder + 'gpt2_classification_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
+#     while alteration_ratio <= 0.04:
+#         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
+#                                    prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+#                                    embeddings_model='gpt2', epochs=100)
+#         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+#         results.flush()
+#         alteration_ratio = round(alteration_ratio + 0.01, 2)
+#
+# # GPT regression
+# alteration_ratio = 0.02
+# with open(results_folder + 'gpt2_regression_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
+#     while alteration_ratio <= 0.04:
+#         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="regression",
+#                                            prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
+#                                            embeddings_model='gpt2', epochs=100)
+#         results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(alteration_ratio, f1, precision, recall))
+#         results.flush()
+#         alteration_ratio = round(alteration_ratio + 0.01, 2)
 
 
 
 # GPT regression
 alteration_ratio = 0.02
 with open(results_folder + 'xl_regression_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-    while alteration_ratio <= 0.04:
+    while alteration_ratio <= 0.02:
         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="regression",
                                            prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
                                            embeddings_model='gpt2', epochs=100)
@@ -64,7 +64,7 @@ with open(results_folder + 'xl_regression_transfer_results_anomaly_ratio_0.05.tx
 # GPT regression
 alteration_ratio = 0.02
 with open(results_folder + 'xl_multiclass_transfer_results_anomaly_ratio_0.05.txt', 'w') as results:
-    while alteration_ratio <= 0.04:
+    while alteration_ratio <= 0.02:
         f1, precision, recall = experiment(anomaly_type='duplicate_lines', anomaly_amount=1, mode="multiclass",
                                            prediction_only=False, anomaly_ratio=0.05, alteration_ratio=alteration_ratio,
                                            embeddings_model='gpt2', epochs=100)
