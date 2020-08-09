9f3e010ee473101e1fb410eafea5fe919d50a46e
Harald Ott <>
2020-08-09 11:22:28 +0200

    move retreival of embeddings to gpu, and put in print statements to see where we're stuck

--- wordembeddings/transform_gpt_2.py
+++ wordembeddings/transform_gpt_2.py
@@ -41,6 +41,7 @@ def _prepare_vectors(templates, pretrained_weights, tokenizer_class, model_class
 
     input_ids = [tokenizer.convert_tokens_to_ids(sentence) for sentence in tokenized_text]
     tokens_tensors = [torch.tensor([idx_tokens]) for idx_tokens in input_ids]
+    tokens_tensors = torch.cat(tokens_tensors, dim=0)
     tokens_tensors.to(device)
     with torch.no_grad():
         encoded_layers = [model(t)[0] for t in tokens_tensors]
