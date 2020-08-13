60f06ecac9ba16d827c8610901c1a0203bbb2686
Harald Ott <>
2020-08-13 11:52:43 +0200

    fix experiment replace half

--- experiment_replace_half.py
+++ experiment_replace_half.py
@@ -9,7 +9,7 @@ makedirs(results_folder, exist_ok=True)
 
 with open(results_folder + 'bert_multiclass_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
-                               prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                               prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                embeddings_model='bert', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
@@ -17,7 +17,7 @@ with open(results_folder + 'bert_multiclass_anomaly_only.txt', 'a') as results:
 
 with open(results_folder + 'bert_regression_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="regression",
-                               prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                               prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                embeddings_model='bert', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
@@ -27,14 +27,14 @@ with open(results_folder + 'bert_regression_anomaly_only.txt', 'a') as results:
 
 with open(results_folder + 'gpt2_multiclass_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
-                                       prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                                       prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                        embeddings_model='gpt2', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
 
 with open(results_folder + 'gpt2_regression_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="regression",
-                                       prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                                       prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                        embeddings_model='gpt2', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
@@ -45,14 +45,14 @@ with open(results_folder + 'gpt2_regression_anomaly_only.txt', 'a') as results:
 
 with open(results_folder + 'xl_multiclass_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="multiclass",
-                                       prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                                       prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                        embeddings_model='xl', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
 
 with open(results_folder + 'xl_regression_anomaly_only.txt', 'a') as results:
     f1, precision, recall = experiment(anomaly_type='random_lines', anomaly_amount=1, mode="regression",
-                                       prediction_only=True, anomaly_ratio=0, alteration_ratio=0.05,
+                                       prediction_only=True, anomaly_ratio=0.05, alteration_ratio=0.05,
                                        embeddings_model='xl', epochs=60)
     results.write("{:.2f},{:.2f},{:.2f},{:.2f}\n".format(0.05, f1, precision, recall))
     results.flush()
